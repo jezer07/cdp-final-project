3c205a48ade1c213105592eacf11772948b0f773
Jezer Crespo <jezercrespo@gmail.com>
2023-10-09 16:23:11 +0000

    added utils

---
+++ nft/policy/utils.sh
@@ -0,0 +1,3 @@
+# Address: addr_test1vpqzpp3mr7m9h3wanvsy87wer0yl93q0rd378sf4mmwnl6qxzusts
+#query utxo of the address
+cardano-cli query utxo --address addr_test1vpqzpp3mr7m9h3wanvsy87wer0yl93q0rd378sf4mmwnl6qxzusts --testnet-magic 2
